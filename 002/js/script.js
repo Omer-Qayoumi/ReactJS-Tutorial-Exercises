ceaa10ff0a4e0b6d81f65118468280b7c906d581
Omer Qayoumi <omerqayoumi1914@gmail.com>
2024-03-22 19:58:48 +0430

    var let const added

---
+++ 002/js/script.js
@@ -0,0 +1,55 @@
+// var nam1 = 10;
+// let nam2 = 20;
+// console.log(nam1);
+// console.log(nam2);
+//
+// nam1 = 10;
+// console.log(nam1);
+// var nam1;
+//
+// nam2 = 20;
+// console.log(nam2);
+// let nam2;
+//
+//
+// var nam1 = 10;
+// {
+//   var nam1 = 12;
+// }
+// console.log(nam1);
+//
+// let nam1 = 10;
+// {
+//   let nam1 = 12;
+// }
+// console.log(nam1);
+//
+// var message = "say Hi";
+// var score = 12;
+// if(score<15){
+//   var message = "say Good boy"
+// }
+// console.log(message)
+//
+// let message = "say Hi";
+// let score = 12;
+// if (score < 15) {
+//   let message = "say Good boy";
+// }
+// console.log(message);
+//
+// var nam1 = 10;
+// let nam2 = 20;
+// const nam3 = 30;
+// nam1 = 15;
+// nam2 = 25;
+// nam3 = 35;              //--------------------erorr
+// console.log(nam1);
+// console.log(nam2);
+// console.log(nam3);
+//
+// nam1 = 10;
+// {
+//   var nam1 = 15
+// }
+// console.log(nam1)
