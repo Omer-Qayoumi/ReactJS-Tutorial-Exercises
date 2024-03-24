0afa479a7e0df297cb9de78c1488f694eea51905
Omer Qayoumi <omerqayoumi1914@gmail.com>
2024-03-24 14:25:51 +0430

    ternary condition

---
+++ 008/js/script.js
@@ -0,0 +1,11 @@
+// let nubmer1 = 15;
+// let number2 = 5;
+// if (nubmer1 + number2 === 20) {
+//   alert("True");
+// } else {
+//   alert("False");
+// }
+//
+let nubmer1 = 15;
+let number2 = 5;
+nubmer1 + number2 === 20 ? alert("True") : alert("False");
