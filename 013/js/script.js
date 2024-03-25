bb54286540a6833b02b9ba23daced42d6b373920
Omer Qayoumi <omerqayoumi1914@gmail.com>
2024-03-25 19:36:36 +0430

    class

---
+++ 013/js/script.js
@@ -0,0 +1,21 @@
+// function Car(color, name, taitCount) {
+//   this.color = color;
+//   this.name = name;
+//   this.taitCount = taitCount;
+// }
+// let corolla1993 = new Car("white", "Toyota corolla 1993", 4);
+// let corolla1997 = new Car("black", "Toyota corolla 1999", 4);
+// console.log(corolla1993);
+// console.log(corolla1997);
+//
+class Car {
+  constructor(color, name, taitCount) {
+    this.color = color;
+    this.name = name;
+    this.taitCount = taitCount;
+  }
+}
+let markx = new Car("black", "toyota mark x", 4);
+let entop = new Car("black", "mada 9", 4);
+console.log(markx);
+console.log(entop);
